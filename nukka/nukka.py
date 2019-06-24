9d97680a253ca314281cb4ad1814531216ad4527
Byeonggon Lee <gonny952@gmail.com>
2019-06-25 04:04:19 +0900

    First attempt to parse

--- nukka/nukka.py
+++ nukka/nukka.py
@@ -1,5 +1,16 @@
+from lark import Lark
+
 def main():
-  print('hello nukka')
+  f = open('nukka/c.lark')
+  l = Lark(f.read(), ambiguity='explicit')
+  print(l.parse(
+  '''
+  int main() {
+    int a = 200;
+    a = 100;
+  }
+  ''').pretty())
+  f.close()
 
 if __name__ == '__main__':
   main()
